422d881e131a5ce4a90d2142640598ee47fe95df
toothless13 <50870607+toothless13@users.noreply.github.com>
2023-02-10 15:01:02 +0000

    Fix sortByOptions object

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions {
+const sortByOptions = {
     'Best Match': 'best_match',
     'Highest Rated': 'rating',
     'Most Reviewed': 'review_count'
